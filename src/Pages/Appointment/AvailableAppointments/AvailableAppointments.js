dd3c6486f712dac698f15e97c322c70503e6058b
Shapna <shapna088@gmail.com>
2022-11-13 04:34:15 +0600

    add

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -29,6 +29,7 @@ const AvailableAppointments = ({ selectedDate }) => {
                 treatment &&
                 <BookingModal
                     treatment={treatment}
+                    selectedDate={selectedDate}
                 ></BookingModal>
             }
         </section >
