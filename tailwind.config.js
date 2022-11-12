4bb6b04318f40ba3a79503f9ec8c96a730884b57
Shapna <shapna088@gmail.com>
2022-11-12 15:07:23 +0600

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,23 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+
+          secondary: "#19D3AE",
+
+          accent: "#3A4256",
+
+          neutral: "#191D24",
+
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
