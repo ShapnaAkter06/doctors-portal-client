ffc6b5828f1ee20062c0c6215eb9b4d4465b1644
Shapna <shapna088@gmail.com>
2022-11-13 16:37:39 +0600

    add

--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -3,8 +3,19 @@ import { setDefaultOptions } from 'date-fns/esm';
 import React from 'react';
 
 const BookingModal = ({ treatment, selectedDate }) => {
-    const { name, slots } = treatment; //treatment is appointment Options just different name
-    const date = format(selectedDate, 'PP')
+    //treatment is just another name of appointment Options with name, slots, _id
+    const { name, slots } = treatment;
+    const date = format(selectedDate, 'PP');
+
+    const handleBooking = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const slot = form.slot.value;
+        const phone = form.phone.value;
+        console.log(date, name, email, slot, phone);
+    }
     return (
         <>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -12,17 +23,18 @@ const BookingModal = ({ treatment, selectedDate }) => {
                 <div className="modal-box relative">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">{name}</h3>
-                    <form className='grid grid-cols-1 gap-3 mt-10'>
+                    <form
+                        onSubmit={handleBooking}
+                        className='grid grid-cols-1 gap-3 mt-10'>
                         <input type="text" disabled value={date} className="input w-full input-bordered" />
-                        <select className="select select-bordered w-full">
+                        <select name='slot' className="select select-bordered w-full">
                             {
-                                slots.map(slot=> <option value={slot}>{slot}</option>)
+                                slots.map(slot => <option value={slot}>{slot}</option>)
                             }
                         </select>
-                        <input type="text" placeholder='' className="input w-full input-bordered" />
-                        <input type="text" placeholder='' className="input w-full input-bordered" />
-                        <input type="text" placeholder='' className="input w-full input-bordered" />
-                        <input type="text" placeholder='' className="input w-full input-bordered" />
+                        <input name='name' type="name" placeholder='Your name' className="input w-full input-bordered" />
+                        <input name='email' type="email" placeholder='Email Address' className="input w-full input-bordered" />
+                        <input name='phone' type="text" placeholder='Phone Number' className="input w-full input-bordered" />
                         <br />
                         <input className='w-full btn btn-accent' type="submit" value="Submit" />
                     </form>
