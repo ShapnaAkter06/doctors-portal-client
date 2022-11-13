401fd861dfc51b412b01c689a43a61e2d4f4f655
Shapna <shapna088@gmail.com>
2022-11-14 01:43:03 +0600

    add

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-    const { register, handleSubmit } = useForm();
-    
-    const handleLogin = data =>{
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const handleLogin = data => {
         console.log(data)
     }
 
@@ -19,14 +18,25 @@ const Login = () => {
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="text"  {...register("email")} className="input input-bordered w-full max-w-xs" />
+                        <input type="text"
+                            {...register("email", {
+                                required: "Email Address is required"
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-700 mt-2' >{errors.email?.message}</p>}
                     </div>
 
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password"  {...register("password")} className="input input-bordered w-full max-w-xs" />
+                        <input type="password"
+                            {...register("password", {
+                                required: "Password is required",
+                                minLength: {value: 6, message: "Password must be at least 6 characters"}
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                        {errors.password && <p className='text-red-700 mt-2' >{errors.password?.message}</p>}
                         <label className="label my-2">
                             <span className="label-text">Forget Password?</span>
                         </label>
